16ff9e62de60c7ee4177a6240e20ba74d603aad6
Veera <veerakuraganti3@gmail.com>
2026-06-11 10:19:01 +0530

    tarcking page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import {CheckoutPage} from './pages/Checkoutpage'
+import {OrdersPage} from './pages/OrdersPage'
 import './App.css';
+import { TrackingPage } from './pages/TrackingPage';
 
 function App() {
   return (
       <Routes>
        <Route index element={<HomePage />} />
        <Route path='checkout' element={<CheckoutPage />}></Route>
+       <Route path='orders' element={<OrdersPage />}></Route>
+       <Route path='tracking' element={<TrackingPage/>}></Route>
       </Routes>
   );
 }
